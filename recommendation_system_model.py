da8887cfe6f2c490ad9b9d073acb6c60df71bb2e
Yash Singavi <103949573+YashSingavi@users.noreply.github.com>
2024-05-12 01:04:38 +0530

    Update recommendation_system_model.py

--- recommendation_system_model.py
+++ recommendation_system_model.py
@@ -78,9 +78,9 @@ def data_generator(df, chunksize=10000):
         yield df.iloc[i:i+chunksize]
 
 # Loading datasets
-games_df = reduce_memory(pd.read_csv('games.csv'))
-users_df = reduce_memory(pd.read_csv('users.csv'))
-recommendations_df = reduce_memory(pd.read_csv('recommendations.csv'))
+games_df = reduce_memory(pd.read_csv(r'games.csv'))
+users_df = reduce_memory(pd.read_csv(r'users.csv'))
+recommendations_df = reduce_memory(pd.read_csv(r'recommendations.csv'))
 
 # Function to get similar users
 def get_similar_users(user_id, user_user_matrix, knn_model, n_neighbors=6):
